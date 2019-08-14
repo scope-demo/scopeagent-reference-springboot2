c9208192e32734d14a9eaee49884c138e4edd27f
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2019-08-14 14:51:23 +0200

    Added unit/integration tests service name

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,7 @@
-./mvnw -fae verify -Pjava11
+export SCOPE_SERVICE=unitTests
+echo "---------- Executing $SCOPE_SERVICE"
+./mvnw -fae clean test -Pjava11 -DskipITs=true
+
+export SCOPE_SERVICE=integrationTests
+echo "---------- Executing $SCOPE_SERVICE"
+./mvnw -fae clean integration-test -Pjava11 -DskipUTs=true
