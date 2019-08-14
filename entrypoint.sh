ac29dd820bbd21e0a16a5b7571b2b9f945d7f30a
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2019-08-14 15:04:23 +0200

    Added IS_CI envVar

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,16 @@
-export SCOPE_SERVICE=unitTests
-echo "---------- Executing $SCOPE_SERVICE"
-./mvnw -fae clean test -Pjava11 -DskipITs=true
+if [ "$IS_CI" = "" ]; then
+  echo "----------- IS_CI is empty"
+  export SCOPE_SERVICE=unitTests
+  echo "---------- Executing $SCOPE_SERVICE"
+  ./mvnw -fae clean test -Pjava11 -DskipITs=true
 
-export SCOPE_SERVICE=integrationTests
-echo "---------- Executing $SCOPE_SERVICE"
-./mvnw -fae clean integration-test -Pjava11 -DskipUTs=true
+  export SCOPE_SERVICE=integrationTests
+  echo "---------- Executing $SCOPE_SERVICE"
+  ./mvnw -fae clean integration-test -Pjava11 -DskipUTs=true
+else
+  echo "----------- IS_CI NOT is empty"
+  export SCOPE_SERVICE=default
+  ./mvnw -fae clean verify -Pjava11
+fi
+
+
